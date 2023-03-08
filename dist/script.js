4a08160ead9c0f5b677c355ac431674b2680bb11
kcryns <rayneskarlcydrick@gmail.com>
2023-03-08 15:39:45 +0800

    SASS-STARBUCKS

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,7 @@
-const toggleMenu = document.querySelector(".toggle__menu");
-const headernav = document.querySelector(".header__nav");
-toggleMenu.addEventListener("click", () => {
-  toggleMenu.classList.toggle("open");
-  headernav.classList.toggle("open");
-});
+const kamote = document.querySelectorAll(".links__item h4")
+kamote.forEach((kamoteq) => {
+    kamoteq.addEventListener("click", () => {
+        kamoteq.nextElementSibling.classList.toggle("open");
+        kamoteq.querySelector("i").classList.toggle("open");
+    })
+})
