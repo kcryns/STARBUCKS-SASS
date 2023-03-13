a71a06937dafe856b7c4c6ba4e8e605b48e06e33
kcryns <rayneskarlcydrick@gmail.com>
2023-03-13 11:15:09 +0800

    sb

--- dist/script.js
+++ dist/script.js
@@ -5,3 +5,10 @@ kamote.forEach((kamoteq) => {
         kamoteq.querySelector("i").classList.toggle("open");
     })
 })
+
+const toggleMenu = document.querySelector(".toggle__menu");
+const headernav = document.querySelector(".header__nav1");
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headernav.classList.toggle("open");
+});
